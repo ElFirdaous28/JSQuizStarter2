59dc2ec9b5a8f87218d904df3380c98c02e274db
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2025-09-24 11:53:36 +0100

    display question -handle quiz

--- js/storage.js
+++ js/storage.js
@@ -21,3 +21,35 @@ export function getUsers() {
 export function saveChoosedTheme(choice) {
     localStorage.setItem("selectedTheme", choice);
 }
+
+export function saveUserAnswer(currentUser, selectedTheme, questionId, userAnswers) {
+    let users = getUsers();
+    if (!users[currentUser]) return;
+
+    if (!users[currentUser].themes[selectedTheme]) {
+        users[currentUser].themes[selectedTheme] = { answers: [], score: 0, totalTime: 0 };
+    }
+
+    let answers = users[currentUser].themes[selectedTheme].answers;
+
+    let existing = answers.find(answer => answer.questionId === questionId);
+
+    if (existing) {
+        existing.userAnswers = userAnswers;
+    } else {
+        answers.push({ questionId, userAnswers });
+    }
+
+    localStorage.setItem("users", JSON.stringify(users));
+}
+
+export function saveScoreDate(currentUser, score, selectedTheme) {
+    let users = getUsers();
+
+    users[currentUser].themes[selectedTheme].score = score;
+    users[currentUser].themes[selectedTheme].dateTime = new Date().toISOString();
+
+
+    localStorage.setItem("users", JSON.stringify(users));
+
+}
